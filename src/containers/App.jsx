4c37e140aede63a9ba37d2ce2078abca77fb1155
Hector Flores <Hector_Flores@eogresources.com>
2020-04-19 13:02:40 -0700

    Header and Search structure and styles

---
+++ src/containers/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+import Search from '../components/Search';
+
+import '../assets/styles/App.scss';
+
+const App = () => (
+  <div className='app'>
+    <Header />
+    <Search />
+  </div>
+);
+
+export default App;
