0d6b4084dce9dd12d36fa79560c2b9b053d2cb70
Hector Flores <Hector_Flores@eogresources.com>
2020-04-19 20:28:05 -0700

    Structure and styles for Carousel, CarouselItem

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
 
 import '../assets/styles/App.scss';
 
@@ -8,6 +11,14 @@ const App = () => (
   <div className='app'>
     <Header />
     <Search />
+    <Categories>
+      <Carousel>
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+    </Categories>
   </div>
 );
 
