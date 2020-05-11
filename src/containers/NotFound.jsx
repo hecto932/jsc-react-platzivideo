e83458296f0cea338a862ccc6314ac1469870a90
Hector Flores <Hector_Flores@eogresources.com>
2020-05-10 20:23:28 -0700

    Not found

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotFound = () => (
+  <>
+    <h1>No encontrado</h1>
+    <h2>Regresar a home</h2>
+  </>
+);
+
+export default NotFound;
--- src/routes/App.js
+++ src/routes/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from '../containers/Home';
 import Login from '../containers/Login';
 import Register from '../containers/Register';
+import NotFound from '../containers/NotFound';
 
 const App = () => (
   <BrowserRouter>
@@ -11,6 +12,7 @@ const App = () => (
       <Route exact path='/' component={Home} />
       <Route exact path='/login' component={Login} />
       <Route exact path='/register' component={Register} />
+      <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
 );
