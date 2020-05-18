7e4163aeebd9c5fbc8bb199c373a2828c2ab6e6f
Hector Flores <Hector_Flores@eogresources.com>
2020-05-17 19:34:12 -0700

    Fixing player functionality

--- src/containers/Player.jsx
+++ src/containers/Player.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import '../assets/styles/components/Player.scss';
 
 const Player = (props) => {
+  const { id } = props.match.params;
   return (
     <div className='Player'>
       <video controls autoPlay>
