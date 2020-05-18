59537b2a337b2101be3d7813eb655e7e0f679a61
Hector Flores <Hector_Flores@eogresources.com>
2020-05-17 19:25:18 -0700

    General view of player

---
+++ src/containers/Player.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import '../assets/styles/components/Player.scss';
+
+const Player = () => {
+  return (
+    <div className='Player'>
+      <video controls autoPlay>
+        <source src='' type='video/mp4' />
+      </video>
+      <div className='Player-back'>
+        <button type='button'>Regresar</button>
+      </div>
+    </div>
+  );
+};
+
+export default Player;
