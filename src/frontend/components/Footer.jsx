c97a1c304dd5a015cb5b893e6e5c93463ee6a1c2
Hector Flores <Hector_Flores@eogresources.com>
2020-09-12 19:13:24 -0700

    Removing hello from footer

--- src/frontend/components/Footer.jsx
+++ src/frontend/components/Footer.jsx
@@ -8,7 +8,6 @@ const Footer = () => (
     <a href="/">Terminos de uso</a>
     <a href="/">Declaración de privacidad</a>
     <a href="/">Centro de ayuda</a>
-    <span>Hola</span>
   </footer>
 );
 
